d1fc1685dbb70650251fb08c2f4a7f9beed68261
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-03-12 03:44:22 +0900

    Add directives, labels, and instructions parsing

---
+++ Public/simple-mips-asm/Parsing.hh
@@ -0,0 +1,151 @@
+// Copyright (c) 2021 Chanjung Kim (paxbun). All rights reserved.
+// Licensed under the MIT License.
+
+#ifndef SIMPLE_MIPS_ASM_PARSE_HH
+#define SIMPLE_MIPS_ASM_PARSE_HH
+
+#include <simple-mips-asm/Formats.hh>
+#include <simple-mips-asm/Tokenization.hh>
+
+#include <string_view>
+#include <variant>
+#include <vector>
+
+// ----------------------------------- Fragment definitions ------------------------------------ //
+
+struct DataDirData
+{};
+
+struct TextDirData
+{};
+
+struct WordDirData
+{
+    uint32_t value;
+};
+
+struct LabelData
+{
+    std::string_view value;
+};
+
+struct RFormatData
+{
+    uint8_t         source1;
+    uint8_t         source2;
+    uint8_t         destination;
+    RFormatFunction function;
+};
+
+struct JRFormatData
+{
+    uint8_t          source;
+    JRFormatFunction function;
+};
+
+struct SRFormatData
+{
+    uint8_t          source;
+    uint8_t          destination;
+    uint8_t          shiftAmount;
+    SRFormatFunction function;
+};
+
+struct IFormatData
+{
+    IFormatOperation operation;
+    uint8_t          destination;
+    uint8_t          source;
+    uint16_t         immediate;
+};
+
+struct BIFormatData
+{
+    BIFormatOperation operation;
+    uint8_t           destination;
+    uint8_t           source;
+    std::string_view  target;
+};
+
+struct IIFormatData
+{
+    IIFormatOperation operation;
+    uint8_t           destination;
+    uint16_t          immediate;
+};
+
+struct OIFormatData
+{
+    OIFormatOperation operation;
+    uint8_t           operand1;
+    uint8_t           operand2;
+    uint16_t          offset;
+};
+
+struct JFormatData
+{
+    JFormatOperation opeartion;
+    std::string_view target;
+};
+
+struct LAFormatData
+{
+    LAFormatType     type;
+    std::string_view target;
+};
+
+// clang-format off
+using FragmentData = std::variant<
+    // directives
+    DataDirData,  TextDirData,  WordDirData,
+    // labels
+    LabelData,
+    // instructions
+    RFormatData,  JRFormatData, SRFormatData,
+    IFormatData,  BIFormatData, IIFormatData,
+    OIFormatData, JFormatData,  LAFormatData>;
+// clang-format on
+
+/// <summary>
+/// Represents a part of code which is an instruction, a label, or a directive.
+/// </summary>
+struct Fragment
+{
+    FragmentData data;
+    Range        range;
+};
+
+/// <summary>
+/// Represents an error occured during the parsing phase.
+/// </summary>
+struct ParseError
+{
+    enum class Type
+    {
+        UnexpectedToken,
+        UnexpectedEof,
+        UnexpectedValue,
+    };
+
+    Type  type;
+    Range range;
+};
+
+/// <summary>
+/// Represents a parsing result.
+/// </summary>
+struct ParseResult
+{
+    std::vector<Fragment>   fragments;
+    std::vector<ParseError> errors;
+};
+
+/// <summary>
+/// Parses the given array of tokens. The given array's lifetime must be equal to or longer than
+/// that of fragments.
+/// </summary>
+/// <param name="tokens">the array of tokens to parse</param>
+/// <returns>parsing result</returns>
+ParseResult Parse(std::vector<Token> const& tokens);
+
+#endif
---
+++ Source/Parsing.cc
@@ -0,0 +1,458 @@
+// Copyright (c) 2021 Chanjung Kim (paxbun). All rights reserved.
+// Licensed under the MIT License.
+
+#include <simple-mips-asm/Formats.hh>
+#include <simple-mips-asm/Parsing.hh>
+
+#include <cctype>
+#include <charconv>
+#include <limits>
+#include <string_view>
+#include <unordered_map>
+
+using namespace std::literals::string_view_literals;
+
+namespace
+{
+
+// ----------------------------------------  Constants ----------------------------------------- //
+
+constexpr uint8_t NumRegisters = 32;
+
+// ----------------------------------------  Utilities ----------------------------------------- //
+
+using Iterator = std::vector<Token>::const_iterator;
+
+struct CaseInsensitiveHash
+{
+    constexpr size_t operator()(std::string_view const& lhs) const
+    {
+        size_t rtn = _fnvOffsetBasis;
+
+        for (char c : lhs)
+        {
+            rtn ^= size_t(toupper(c));
+            rtn *= _fnvPrime;
+        }
+
+        return rtn;
+    }
+
+  private:
+    constexpr static size_t _fnvOffsetBasis = 0xCBF29CE484222325;
+    constexpr static size_t _fnvPrime       = 0x00000100000001B3;
+};
+
+bool CompareCaseInsensitive(std::string_view const& lhs, std::string_view const& rhs)
+{
+    if (lhs.size() != rhs.size()) return false;
+
+    for (size_t i = 0; i < lhs.size(); ++i)
+        if (toupper(lhs[i]) != toupper(rhs[i])) return false;
+
+    return true;
+}
+
+template <typename LeftT, typename... ValueTs>
+bool IsOneOf(LeftT left, ValueTs... valuesToCompare)
+{
+    return ((left == valuesToCompare) && ...);
+}
+
+template <typename T>
+bool GetInteger(Iterator current, T& output)
+{
+    size_t integerValue = 0;
+    if (current->type == Token::Type::Integer)
+    {
+        auto stringValue = current->value;
+        std::from_chars(stringValue.begin(), stringValue.end(), integerValue, 10);
+    }
+    else if (current->type == Token::Type::HexInteger)
+    {
+        auto stringValue = current->value.substr(2);
+        std::from_chars(stringValue.begin(), stringValue.end(), integerValue, 16);
+    }
+    else
+    {
+        return false;
+    }
+
+    if (integerValue >= std::numeric_limits<T>::max()) return false;
+
+    output = static_cast<T>(integerValue);
+    return true;
+}
+
+// ---------------------------------  Instruction name tables ---------------------------------- //
+
+template <typename T>
+using InstructionTable = std::unordered_map<std::string_view, T, CaseInsensitiveHash>;
+
+InstructionTable<RFormatFunction> const _rFormatTable {
+    { "ADDU"sv, RFormatFunction::ADDU }, { "SUBU"sv, RFormatFunction::SUBU },
+    { "AND"sv, RFormatFunction::AND },   { "OR"sv, RFormatFunction::OR },
+    { "NOR"sv, RFormatFunction::NOR },   { "SLTU"sv, RFormatFunction::SLTU },
+};
+
+InstructionTable<JRFormatFunction> const _jrFormatTable {
+    { "JR"sv, JRFormatFunction::JR },
+};
+
+InstructionTable<SRFormatFunction> const _srFormatTable {
+    { "SLL"sv, SRFormatFunction::SLL },
+    { "SRL"sv, SRFormatFunction::SRL },
+};
+
+InstructionTable<IFormatOperation> const _iFormatTable {
+    { "ADDIU"sv, IFormatOperation::ADDIU },
+    { "ANDI"sv, IFormatOperation::ANDI },
+    { "ORI"sv, IFormatOperation::ORI },
+    { "SLTIU"sv, IFormatOperation::SLTIU },
+};
+
+InstructionTable<BIFormatOperation> const _biFormatTable {
+    { "BEQ"sv, BIFormatOperation::BEQ },
+    { "BNE"sv, BIFormatOperation::BNE },
+};
+
+InstructionTable<IIFormatOperation> const _iiFormatTable {
+    { "LUI"sv, IIFormatOperation::LUI },
+};
+
+InstructionTable<OIFormatOperation> const _oiFormatTable {
+    { "LB"sv, OIFormatOperation::LB },
+    { "LW"sv, OIFormatOperation::LW },
+    { "SB"sv, OIFormatOperation::SB },
+    { "SW"sv, OIFormatOperation::SW },
+};
+
+InstructionTable<JFormatOperation> const _jFormatTable {
+    { "J"sv, JFormatOperation::J },
+    { "JAL"sv, JFormatOperation::JAL },
+};
+
+InstructionTable<LAFormatType> const _laFormatTable {
+    { "LA"sv, LAFormatType::LA },
+};
+
+// -----------------------------------  Parser output types ------------------------------------ //
+
+/// <summary>
+/// Returned when the parser cannot parse the given token stream.
+/// </summary>
+struct CannotParse
+{
+    ParseError::Type errorType;
+    Iterator         errorAt;
+};
+
+/// <summary>
+/// Returned when the parser can parse the given token stream.
+/// </summary>
+struct CanParse
+{
+    FragmentData data;
+    Iterator     fragmentEnd;
+};
+
+/// <summary>
+/// Union of possible parser output types
+/// </summary>
+using ParserOutput = std::variant<CannotParse, CanParse>;
+
+// -----------------------------------------  Parsers ------------------------------------------ //
+
+// Defines an iterator indicating the current position.
+#define DEFINE_CURRENT Iterator current = begin
+
+// Returns CannotParse with UnexpectedEof.
+#define UNEXPECTED_EOF return CannotParse { ParseError::Type::UnexpectedEof, current };
+
+// Returns CannotParse with UnexpectedToken.
+#define UNEXPECTED_TOKEN return CannotParse { ParseError::Type::UnexpectedToken, current };
+
+// Returns CannotParse with UnexpectedValue.
+#define UNEXPECTED_VALUE return CannotParse { ParseError::Type::UnexpectedValue, current };
+
+// Returns CanParse with the given data.
+#define RESULT(data) return CanParse { data, current + 1 };
+
+// Advances the iterator defined by DEFINE_CURRENT.
+#define ADVANCE_CURRENT                                                                            \
+    {                                                                                              \
+        ++current;                                                                                 \
+        if (current == end) return CannotParse { ParseError::Type::UnexpectedEof, current };       \
+    }
+
+// Advances the iterator until non-whitespace token appears.
+#define SKIP_WHITESPACES                                                                           \
+    while (current->type == Token::Type::Whitespace) ADVANCE_CURRENT
+
+// Advances the iterator until non-whitespace token appears. If that first non-whitespace token's
+// type is not the expected type, returns CannotParse with UnexpectedToken.
+#define EXPECT_NEXT(...)                                                                           \
+    SKIP_WHITESPACES;                                                                              \
+    if (IsOneOf(current->type, __VA_ARGS__)) UNEXPECTED_TOKEN
+
+// Advances the iterator until non-whitespace token appears. If that first non-whitespace token is
+// not a Word with the given value, returns CannotParse with UnexpectedValue.
+#define EXPECT_WORD(ExpectedValue)                                                                 \
+    EXPECT_NEXT(Token::Type::Word);                                                                \
+    if (!CompareCaseInsensitive(current->value, ExpectedValue)) UNEXPECTED_VALUE
+
+// Advances the iterator until non-whitespace token appears. If that first non-whitespace token is
+// not a Word where the value is registered in the given table, returns CannotParse with
+// UnexpectedValue. This macro must be used only once within one function.
+#define EXPECT_OPCODE(Table)                                                                       \
+    EXPECT_NEXT(Token::Type::Word);                                                                \
+    auto it = Table.find(current->value);                                                          \
+    if (it == Table.end()) UNEXPECTED_VALUE;                                                       \
+    auto type = it->second;
+
+// Checks whether the next incoming tokens indicate a register.
+#define EXPECT_REGISTER(OutputVariableName)                                                        \
+    EXPECT_NEXT(Token::Type::Dollar);                                                              \
+    EXPECT_NEXT(Token::Type::Integer);                                                             \
+    uint8_t OutputVariableName;                                                                    \
+    if (!GetInteger(current, OutputVariableName) || NumRegisters <= OutputVariableName)            \
+        UNEXPECTED_VALUE;
+
+#define EXPECT_IMM(OutputVariableName)                                                             \
+    EXPECT_NEXT(Token::Type::Integer);                                                             \
+    uint32_t OutputVariableName;                                                                   \
+    if (!GetInteger(current, OutputVariableName)) UNEXPECTED_VALUE;
+
+// DataDirective: Dot + "data"
+ParserOutput Data(Iterator begin, Iterator end)
+{
+    DEFINE_CURRENT;
+
+    EXPECT_NEXT(Token::Type::Dot);
+    ADVANCE_CURRENT;
+    EXPECT_WORD("data");
+
+    RESULT(DataDirData {});
+}
+
+// TextDirective: Dot + "text"
+ParserOutput Text(Iterator begin, Iterator end)
+{
+    DEFINE_CURRENT;
+
+    EXPECT_NEXT(Token::Type::Dot);
+    ADVANCE_CURRENT;
+    EXPECT_WORD("text");
+
+    RESULT(TextDirData {});
+}
+
+// WordDirective: Dot + "word" + (Integer | HexInteger)
+ParserOutput Word(Iterator begin, Iterator end)
+{
+    DEFINE_CURRENT;
+
+    EXPECT_NEXT(Token::Type::Dot);
+    ADVANCE_CURRENT;
+    EXPECT_WORD("word");
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Integer, Token::Type::HexInteger);
+
+    uint32_t result;
+    if (!GetInteger<uint32_t>(current, result)) UNEXPECTED_VALUE;
+
+    RESULT(WordDirData { result });
+}
+
+// Label: Word + Colon
+ParserOutput Label(Iterator begin, Iterator end)
+{
+    DEFINE_CURRENT;
+
+    EXPECT_NEXT(Token::Type::Word);
+    auto labelName = current->value;
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Colon);
+
+    RESULT(LabelData { labelName });
+}
+
+// RFormatInstruction: RFormatOpcode + Register + Register + Register
+ParserOutput RFormatInstruction(Iterator begin, Iterator end)
+{
+    DEFINE_CURRENT;
+
+    EXPECT_OPCODE(_rFormatTable);
+    ADVANCE_CURRENT;
+    EXPECT_REGISTER(source1);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Comma);
+    ADVANCE_CURRENT;
+    EXPECT_REGISTER(source2);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Comma);
+    ADVANCE_CURRENT;
+    EXPECT_REGISTER(destination);
+
+    RESULT((RFormatData { source1, source2, destination, type }));
+}
+
+// JRFormatInstruction: JRFormatOpcode + Register
+ParserOutput JRFormatInstruction(Iterator begin, Iterator end)
+{
+    DEFINE_CURRENT;
+
+    EXPECT_OPCODE(_jrFormatTable);
+    ADVANCE_CURRENT;
+    EXPECT_REGISTER(source);
+
+    RESULT((JRFormatData { source, type }));
+}
+
+// SRFormatInstruction: SRFormatOpcode + Register + Register + (Integer | HexInteger)
+ParserOutput SRFormatInstruction(Iterator begin, Iterator end)
+{
+    DEFINE_CURRENT;
+
+    EXPECT_OPCODE(_srFormatTable);
+    ADVANCE_CURRENT;
+    EXPECT_REGISTER(source);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Comma);
+    ADVANCE_CURRENT;
+    EXPECT_REGISTER(destination);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Comma);
+    ADVANCE_CURRENT;
+    EXPECT_IMM(shiftAmount);
+
+    if (shiftAmount >= 32) UNEXPECTED_VALUE;
+
+    RESULT((SRFormatData { source, destination, static_cast<uint8_t>(shiftAmount), type }));
+}
+
+// IFormatInstruction: IFormatOpcode + Register + Register + (Integer | HexInteger)
+ParserOutput IFormatInstruction(Iterator begin, Iterator end)
+{
+    DEFINE_CURRENT;
+
+    EXPECT_OPCODE(_iFormatTable);
+    ADVANCE_CURRENT;
+    EXPECT_REGISTER(destination);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Comma);
+    ADVANCE_CURRENT;
+    EXPECT_REGISTER(source);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Comma);
+    ADVANCE_CURRENT;
+    EXPECT_IMM(immediate);
+
+    if (immediate >= std::numeric_limits<uint16_t>::max()) UNEXPECTED_VALUE;
+
+    RESULT((IFormatData { type, destination, source, static_cast<uint16_t>(immediate) }));
+}
+
+// BIFormatInstruction: BIFormatOpcode + Register + Register + Word
+ParserOutput BIFormatInstruction(Iterator begin, Iterator end)
+{
+    DEFINE_CURRENT;
+
+    EXPECT_OPCODE(_biFormatTable);
+    ADVANCE_CURRENT;
+    EXPECT_REGISTER(destination);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Comma);
+    ADVANCE_CURRENT;
+    EXPECT_REGISTER(source);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Comma);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Word);
+    auto target = current->value;
+
+    RESULT((BIFormatData { type, destination, source, target }));
+}
+
+// IIFormatInstruction: IIFormatOpcode + Register + (Integer | HexInteger)
+ParserOutput IIFormatInstruction(Iterator begin, Iterator end)
+{
+    DEFINE_CURRENT;
+
+    EXPECT_OPCODE(_iiFormatTable);
+    ADVANCE_CURRENT;
+    EXPECT_REGISTER(destination);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Comma);
+    ADVANCE_CURRENT;
+    EXPECT_IMM(immediate);
+
+    if (immediate >= std::numeric_limits<uint16_t>::max()) UNEXPECTED_VALUE;
+
+    RESULT((IIFormatData { type, destination, static_cast<uint16_t>(immediate) }));
+}
+
+// OIFormatInstruction: OIFormatOpcode + Register + (Integer | HexInteger) + BracketOpen + Register
+// + BracketClose
+ParserOutput OIFormatInstruction(Iterator begin, Iterator end)
+{
+    DEFINE_CURRENT;
+
+    EXPECT_OPCODE(_oiFormatTable);
+    ADVANCE_CURRENT;
+    EXPECT_REGISTER(operand2);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Comma);
+    ADVANCE_CURRENT;
+    EXPECT_IMM(offset);
+    if (offset >= std::numeric_limits<uint16_t>::max()) UNEXPECTED_VALUE;
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::BracketOpen);
+    ADVANCE_CURRENT;
+    EXPECT_REGISTER(operand1);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::BracketClose);
+
+    RESULT((OIFormatData { type, operand1, operand2, static_cast<uint16_t>(offset) }));
+}
+
+// JFormatInstruction: JFormatOpcode + Word
+ParserOutput JFormatInstruction(Iterator begin, Iterator end)
+{
+    DEFINE_CURRENT;
+
+    EXPECT_OPCODE(_jFormatTable);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Word);
+    auto target = current->value;
+
+    RESULT((JFormatData { type, target }));
+}
+
+// LAFormatInstruction: LAFormatOpcode + Word
+ParserOutput LAFormatInstruction(Iterator begin, Iterator end)
+{
+    DEFINE_CURRENT;
+
+    EXPECT_OPCODE(_laFormatTable);
+    ADVANCE_CURRENT;
+    EXPECT_NEXT(Token::Type::Word);
+    auto target = current->value;
+
+    RESULT((LAFormatData { type, target }));
+}
+
+}
+
+ParseResult Parse(std::vector<Token> const& tokens)
+{
+    std::vector<Fragment>   fragments;
+    std::vector<ParseError> errors;
+
+    auto       begin = tokens.begin();
+    auto const end   = tokens.end();
+
+    while (begin != end) {}
+
+    return { std::move(fragments), std::move(errors) };
+}
