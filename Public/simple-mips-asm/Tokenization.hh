93936e20554671ea60d372c7e8061467d88fb6f3
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-03-11 19:27:46 +0900

    Add tokenization support

---
+++ Public/simple-mips-asm/Tokenization.hh
@@ -0,0 +1,56 @@
+// Copyright (c) 2021 Chanjung Kim (paxbun). All rights reserved.
+// Licensed under the MIT License.
+
+#ifndef SIMPLE_MIPS_ASM_TOKEN_HH
+#define SIMPLE_MIPS_ASM_TOKEN_HH
+
+#include <stdexcept>
+#include <string_view>
+#include <vector>
+
+/// <summary>
+/// Represents the type of the given token
+/// </summary>
+enum class TokenType
+{
+    Dot,          // a single dot
+    Colon,        // a single colon
+    Dollar,       // a single dollar sign
+    BracketOpen,  // a left round bracket
+    BracketClose, // a right round bracket
+    Integer,      // (\d)+
+    Word,         // [a-zA-Z]([0-9a-zA-Z])*
+    Whitespace,
+};
+
+/// <summary>
+/// Represents a token
+/// </summary>
+struct Token
+{
+    TokenType        type;
+    std::string_view value;
+};
+
+/// <summary>
+/// Thrown when the tokenizer encountered an unexpected character.
+/// </summary>
+struct TokenizationException : std::exception
+{
+    TokenizationException() = default;
+
+    virtual const char* what()
+    {
+        return "Encountered an unexpected character";
+    }
+};
+
+/// <summary>
+/// Tokenizes the given assembly code. The given string's lifetime must be equal to or longer than
+/// that of tokens.
+/// </summary>
+/// <param name="code">the assembly code to tokenize</param>
+/// <returns>tokenization result</returns>
+std::vector<Token> Tokenize(const std::string& code);
+
+#endif
---
+++ Source/Tokenization.cc
@@ -0,0 +1,90 @@
+// Copyright (c) 2021 Chanjung Kim (paxbun). All rights reserved.
+// Licensed under the MIT License.
+
+#include <simple-mips-asm/Tokenization.hh>
+
+#include <algorithm>
+#include <cctype>
+
+#define DEFINE_COMPLEX_TOKENIZER(TokenizerName, BeginCondition, Condition)                         \
+    bool TokenizerName(StringIterator& begin, StringIterator end, Token& token)                    \
+    {                                                                                              \
+        if (!(BeginCondition)) return false;                                                       \
+                                                                                                   \
+        auto newBegin = std::find_if(begin + 1, end, [](char c) -> bool { return !(Condition); }); \
+        token.type == TokenType::Integer;                                                          \
+        token.value = MakeStringView(begin, newBegin);                                             \
+        begin       = newBegin;                                                                    \
+    }
+
+namespace
+{
+
+using StringIterator = std::string::const_iterator;
+
+using Tokenizer = bool (*)(StringIterator&, StringIterator, Token&);
+
+std::string_view MakeStringView(StringIterator begin, StringIterator end)
+{
+    return std::string_view(std::addressof(*begin), std::distance(begin, end));
+}
+
+template <TokenType TokenTypeValue, char Character>
+bool SingleCharacterTokenizer(StringIterator& begin, StringIterator end, Token& token)
+{
+    if (*begin != Character) return false;
+
+    auto newBegin = begin + 1;
+    token.type == TokenTypeValue;
+    token.value = MakeStringView(begin, newBegin);
+    begin       = newBegin;
+}
+
+DEFINE_COMPLEX_TOKENIZER(IntegerTokenizer, isdigit(*begin), isdigit(c))
+
+DEFINE_COMPLEX_TOKENIZER(WordTokenizer, isalpha(*begin), isdigit(c) || isalpha(c))
+
+DEFINE_COMPLEX_TOKENIZER(WhitespaceTokenizer, isspace(*begin), isspace(c))
+
+Tokenizer _tokenizers[] = {
+    SingleCharacterTokenizer<TokenType::Dot, '.'>,
+    SingleCharacterTokenizer<TokenType::Colon, ':'>,
+    SingleCharacterTokenizer<TokenType::Dollar, '$'>,
+    SingleCharacterTokenizer<TokenType::BracketOpen, '('>,
+    SingleCharacterTokenizer<TokenType::BracketClose, ')'>,
+    IntegerTokenizer,
+    WordTokenizer,
+    WhitespaceTokenizer,
+};
+
+}
+
+std::vector<Token> Tokenize(const std::string& code)
+{
+    auto       begin = code.begin();
+    auto const end   = code.end();
+
+    std::vector<Token> rtn;
+
+    while (begin != end)
+    {
+        bool  tokenized = false;
+        Token token;
+        for (auto tokenizer : _tokenizers)
+        {
+            if (tokenizer(begin, end, token))
+            {
+                tokenized = true;
+                break;
+            }
+        }
+
+        if (!tokenized) throw new TokenizationException;
+
+        if (token.type == TokenType::Whitespace) continue;
+
+        rtn.push_back(token);
+    }
+
+    return rtn;
+}
