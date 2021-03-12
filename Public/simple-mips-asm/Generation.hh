65f00976bebd2c18afd10a94b8f75695b40e458b
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-03-12 18:55:16 +0900

    Rename Assembler.cc to Generation.cc

--- Public/simple-mips-asm/Generation.hh
+++ Public/simple-mips-asm/Generation.hh
@@ -1,7 +1,7 @@
 // Copyright (c) 2021 Chanjung Kim (paxbun). All rights reserved.
 // Licensed under the MIT License.
 
-#ifndef SIMPLE_MIPS_ASM_ASSEMBLER_HH
-#define SIMPLE_MIPS_ASM_ASSEMBLER_HH
+#ifndef SIMPLE_MIPS_ASM_GENERATOR_HH
+#define SIMPLE_MIPS_ASM_GENERATOR_HH
 
 #endif
--- Source/Generation.cc
+++ Source/Generation.cc
@@ -1,4 +1,4 @@
 // Copyright (c) 2021 Chanjung Kim (paxbun). All rights reserved.
 // Licensed under the MIT License.
 
-#include <simple-mips-asm/Assembler.hh>
+#include <simple-mips-asm/Generation.hh>
